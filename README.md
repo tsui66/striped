ef3f8719707d68ebfe11f27776114212ab5efc10
tsui66 <quinton.xu@gmail.com>
2024-11-30 11:22:22 +0800

    feat: add example directory and update README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# striped-node
+# Striped
+
+Striped is a CLI tool for code-first pricing management in Stripe. Built with Bun and TypeScript.
+
+# Usage
+
+See [example](./example) for a complete example.  
---
+++ example/striped.config.ts
@@ -0,0 +1,72 @@
+import { type PreStripedConfig } from "@usque/striped";
+import { taxCodes } from "@usque/striped";
+
+export const config = {
+  apiVersion: "2024-11-20.acacia",
+  features: {
+    basicAnalytics: "Basic Analytics",
+    aiReporting: "AI Reporting",
+  },
+  products: {
+    hobby: {
+      name: "Hobby Plan",
+      id: "hobby",
+      taxCode: taxCodes.SOFTWARE_AS_A_SERVICE,
+      prices: {
+        monthly: {
+          currency: "usd",
+          amount: 1000,
+          interval: "month",
+          type: "recurring",
+          trialPeriodDays: 7, // Add trial period for monthly plan
+        },
+        lifetime: {
+          currency: "usd",
+          amount: 20000,
+          interval: null,
+          type: "one_time",
+        },
+      },
+      features: ["basicAnalytics"],
+    },
+    pro: {
+      name: "Pro Plan",
+      id: "pro",
+      taxCode: taxCodes.SOFTWARE_AS_A_SERVICE,
+      prices: {
+        annual: {
+          currency: "usd",
+          amount: 20000,
+          interval: "year",
+          type: "recurring",
+        },
+      },
+      features: ["basicAnalytics", "aiReporting"],
+    },
+    enterprise: {
+      name: "Enterprise Plan",
+      id: "enterprise",
+      prices: {
+        annual: {
+          currency: "usd",
+          amount: 20000,
+          interval: "year",
+          type: "recurring",
+          trialPeriodDays: 3, // Add trial period for enterprise plan
+          // tax_code is optional; will default if not specified
+        },
+      },
+      features: ["basicAnalytics", "aiReporting"],
+    },
+  },
+  webhooks: {
+    // tip for vercel use e.g `${process.env.VERCEL_BRANCH_URL}/webhook`
+    // to get auto-deployed 
+    endpoint: "https://striped-example-app.vercel.app/api/webhooks",
+    events: [
+      "checkout.session.completed",
+      "customer.subscription.deleted",
+      "invoice.payment_failed",
+    ],
+  },
+} satisfies PreStripedConfig;
